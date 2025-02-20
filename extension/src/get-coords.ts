6829a34b5f6948ae925a34d0fc704706efb7db1a
Kareem Medhat <kareemmedhatnabil@gmail.com>
2025-02-20 10:04:21 +0200

    lets do it

--- extension/src/get-coords.ts
+++
@@ -1,7 +0,0 @@
-export type Coords = [number, number];
-
-export function getCoords(element: Element): [number, number] {
-  // crossbrowser version
-  var box = element.getBoundingClientRect();
-  return [box.x, box.y];
-}
